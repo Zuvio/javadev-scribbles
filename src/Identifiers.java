a094d550826be72627a35874eeb05a927411ccd2
Erik Romunde <eromunde@hotmail.com>
2018-09-16 13:31:36 +0200

    Added identifiers

---
+++ src/Identifiers.java
@@ -0,0 +1,9 @@
+class Identifiers {
+     public int numbars() {
+         int x = 3;
+         int y;
+         //int z = x + y; //before use, MUST be initiazlized!
+         System.out.println(x);
+         return x;
+     }
+}
